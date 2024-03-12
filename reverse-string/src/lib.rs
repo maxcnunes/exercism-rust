b6b2f2d10c37d676cd9f83be36dc5bb9979aa39e
Max Claus Nunes <maxcnunes@gmail.com>
2024-03-12 12:19:32 -0300

    Support reverse over special chars

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,14 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    // Left this around as an example of how to do it
+    // for general cases where we don't need to support special
+    // unicode chars:
+    //
+    // input.chars().rev().collect()
+
+    // Support special unicode chars.
+    UnicodeSegmentation::graphemes(input, true)
+        .rev()
+        .collect::<String>()
 }
