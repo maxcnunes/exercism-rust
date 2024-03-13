801409535fa7e08200303ed7bcd861d8c4e28a86
Max Claus Nunes <maxcnunes@gmail.com>
2024-03-13 10:19:45 -0300

    Simplify anagram solution

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,15 +1,14 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let (w_lower, w_sorted) = transform(word);
+    let w_ordered = to_ordered_string(word);
 
     let matches = possible_anagrams
         .iter()
         .filter_map(|pa| {
-            let (pa_lower, pa_sorted) = transform(pa);
+            let pa_ordered = to_ordered_string(pa);
 
-            // Check the words are equal after the lower case and sorting transformations
-            if w_lower != pa_lower && w_sorted == pa_sorted {
+            if w_ordered == pa_ordered && word.to_lowercase() != pa.to_lowercase() {
                 Some(pa.to_owned())
             } else {
                 None
@@ -20,19 +19,8 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
     HashSet::from_iter(matches)
 }
 
-fn transform(w: &str) -> (Vec<char>, String) {
-    // Make a copy of the word in lower case, and keep it as a slice of chars
-    let w_lower = w.to_lowercase().chars().collect::<Vec<char>>();
-
-    // Make a new copy of that slice, so we can sort it, but without modifying the previous slice.
-    // This is required so we can compare later the lower case version, without being sorted.
-    let mut w_chars_sort = w_lower.clone();
-
-    // Sort the first slice.
-    w_chars_sort.sort();
-
-    // Transform the slice of chars sorted version into a String.
-    let w_sorted = w_chars_sort.iter().collect::<String>();
-
-    (w_lower, w_sorted)
+fn to_ordered_string(w: &str) -> String {
+    let mut w_ordered = w.to_lowercase().chars().collect::<Vec<char>>();
+    w_ordered.sort();
+    w_ordered.iter().collect::<String>()
 }
