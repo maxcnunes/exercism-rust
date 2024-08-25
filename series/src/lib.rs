a4c4d6846b2e37197c1cac7cabb1e22189bccd90
Max Claus Nunes <maxcnunes@gmail.com>
2024-08-24 21:43:01 -0300

    Improved version based on other comunity solutions

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,17 +1,8 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    let size = digits.len();
-    if len > size {
-        return vec![];
-    }
-
-    // Calculate the total number of series so the
-    // the vector can be initialized with capacity
-    // avoiding any new reallocations later.
-    let total_series = size - len + 1;
-
-    vec!["".to_string(); total_series]
-        .iter()
-        .enumerate()
-        .map(|(i, _)| digits.chars().skip(i).take(len).collect::<String>())
+    digits
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(len)
+        .map(|w| w.iter().collect::<String>())
         .collect()
 }
